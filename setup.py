d055dde6c8a8ff31e971c35fc74b1e1b88abc944
huydo2008 <109401366+huydo2008@users.noreply.github.com>
2022-11-02 20:59:48 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os 
+
+os.system("pkg install python3")
+os.system("pkg install pip3 ")
+os.system("pip3 install requests")
+os.system("pip3 install pysocks")
+os.system("python3 DEATHDOS.py")
